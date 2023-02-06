57501e6be4cc575577e58dd09c17a8936e929884
Jvllmyr <j.vllmyr@gmail.com>
2023-02-06 16:26:06 -0500

    Updated js functions to only trigger in the appropriate media query

--- js/index.js
+++ js/index.js
@@ -1,51 +1,66 @@
 
+// Only trigger these js events while > 600 px max width
+const mediaQueryMinWidth = window.matchMedia('(min-width: 601px)');
+
 // MM events
-$(".mmInfo").on("mouseenter", function() {
-    $(".mmDescription").removeClass("hidden").addClass("active");
+$(".mmInfo").on("mouseenter", function () {
+    if (mediaQueryMinWidth.matches === true) {
+        $(".mmDescription").removeClass("hidden").addClass("active");
+    }
 })
 
-$(".mmInfo").on("mouseleave", function() {
-    $(".mmDescription").removeClass("active").addClass("hidden");
+$(".mmInfo").on("mouseleave", function () {
+    if (mediaQueryMinWidth.matches === true) {
+        $(".mmDescription").removeClass("active").addClass("hidden");
+    }
 })
 
 // CCA events
-$(".ccaInfo").on("mouseenter", function() {
-    $(".ccaDescription").removeClass("hidden").addClass("active");
+$(".ccaInfo").on("mouseenter", function () {
+    if (mediaQueryMinWidth.matches === true) {
+        $(".ccaDescription").removeClass("hidden").addClass("active");
+    }
 })
 
-$(".ccaInfo").on("mouseleave", function() {
-    $(".ccaDescription").removeClass("active").addClass("hidden");
+$(".ccaInfo").on("mouseleave", function () {
+    if (mediaQueryMinWidth.matches === true) {
+        $(".ccaDescription").removeClass("active").addClass("hidden");
+    }
 })
 
 // CCS events
-$(".ccsInfo").on("mouseenter", function() {
-    $(".ccsDescription").removeClass("hidden").addClass("active");
+$(".ccsInfo").on("mouseenter", function () {
+    if (mediaQueryMinWidth.matches === true) {
+        $(".ccsDescription").removeClass("hidden").addClass("active");
+    }
 })
 
-$(".ccsInfo").on("mouseleave", function() {
-    $(".ccsDescription").removeClass("active").addClass("hidden");
+$(".ccsInfo").on("mouseleave", function () {
+    if (mediaQueryMinWidth.matches === true) {
+        $(".ccsDescription").removeClass("active").addClass("hidden");
+    }
 })
 
-// Media Query Functions
-const mediaQuery = window.matchMedia('(max-width: 600px)')
+// Media Query Functions to always show project descriptions while <= 600px max width
+const mediaQueryMaxWidth = window.matchMedia('(max-width: 600px)');
 
-function checkMediaQuery(e) {
-  // Check if the media query is true
-  if (e.matches) {
+function checkMediaQueryMaxWidth(e) {
+    // Check if the media query is true
+    if (e.matches) {
 
-    $(".mmDescription").removeClass("hidden").addClass("active");
-    $(".ccaDescription").removeClass("hidden").addClass("active");
-    $(".ccsDescription").removeClass("hidden").addClass("active");
-  }
-  else {
-    $(".mmDescription").addClass("hidden").removeClass("active");
-    $(".ccsDescription").addClass("hidden").removeClass("active");
-    $(".ccaDescription").addClass("hidden").removeClass("active");
-  }
+        $(".mmDescription").removeClass("hidden").addClass("active");
+        $(".ccaDescription").removeClass("hidden").addClass("active");
+        $(".ccsDescription").removeClass("hidden").addClass("active");
+    }
+    else {
+        $(".mmDescription").addClass("hidden").removeClass("active");
+        $(".ccsDescription").addClass("hidden").removeClass("active");
+        $(".ccaDescription").addClass("hidden").removeClass("active");
+    }
 }
 
 // Register event listener
-mediaQuery.addListener(checkMediaQuery)
+mediaQueryMaxWidth.addListener(checkMediaQueryMaxWidth)
 
 // Initial check
-checkMediaQuery(mediaQuery)
+checkMediaQueryMaxWidth(mediaQueryMaxWidth)
